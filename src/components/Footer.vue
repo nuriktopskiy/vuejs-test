9f3451ad50550562037ba0f666a49a7c76b0dc25
nuriktopskiy <neznayka.2014@yandex.ru>
2021-12-11 01:33:09 +0300

    fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer">
     <span class="footer__text">Telegram: <a href="tg://resolve?domain=Nurik_178">@nurik_178</a></span>
-    <span class="footer__text">Special for «Банда Умников»</span>
+    <span class="footer__text footer__text--right">Special for «Банда Умников»</span>
   </footer>
 </template>
 
